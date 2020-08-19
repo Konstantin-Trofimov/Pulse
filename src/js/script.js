40efe970a315096748638723d2f76fd03e8af0b1
Konstantim <const.trofimov@gmail.com>
2020-08-19 12:33:29 +0300

    Add modals and mailer

--- src/js/script.js
+++ src/js/script.js
@@ -5,11 +5,11 @@ const slider = tns({
     slideBy: 'page',
     autoplay: false,
     controls: false,
-    nav: false,
+    nav: true,
     responsive: {
         1085: {
             items: 1,
-            nav: true,
+            nav: false,
         },
     }
 });
@@ -44,6 +44,75 @@ $('document').ready(() => {
 
     toggleSlide('.catalog-item__back');
     toggleSlide('.catalog-item__link');
+
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text())
+            $('.overlay, #order').fadeIn('slow');
+        })
+    })
+
+    function validateForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Введиете минимум {0} символа")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введен адрес почты"
+                }
+            }
+        });
+    }
+
+    validateForms('#consultation form');
+    validateForms('#order form');
+    validateForms('#consultation-form');
+
+    $('input[name=phone]').mask("+7(999)-999-9999");
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function () {
+            $(this).find('input').val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
+
+
+
 
 
