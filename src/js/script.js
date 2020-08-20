939a7c52c980b00d6041cf28ecf624ea79d76747
Konstantim <const.trofimov@gmail.com>
2020-08-20 17:05:53 +0300

    Add animations

--- src/js/script.js
+++ src/js/script.js
@@ -110,6 +110,23 @@ $('document').ready(() => {
         });
         return false;
     });
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn('slow');
+        }
+        else {
+            $('.pageup').fadeOut('slow');
+        }
+    });
+
+    $("a[href^='#up']").click(function () {
+        const _href = $(this).attr("href");
+        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+        return false;
+    });
+
+    new WOW().init();
 });
 
 
--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -11,14 +11,14 @@ $mail->CharSet = 'utf-8';
 // $mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = '';                 // Наш логин
-$mail->Password = '';                           // Наш пароль от ящика
+$mail->Username = 'te.st.01@mail.ru';                 // Наш логин
+$mail->Password = '2!gi#n4%00';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'Pulse');   // От кого письмо 
+$mail->setFrom('te.st.01@mail.ru', 'Pulse');   // От кого письмо 
 $mail->addAddress('gocafi7379@arasj.net');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
